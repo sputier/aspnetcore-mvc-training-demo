7b1bbc383e1130d086f6ea85ad597bdca9368b68
Sébastien Putier <sputier@gmail.com>
2019-10-20 22:09:03 +0200

    Amélioration de l'action implémentée précédemment - utilisation d'un repository

---
+++ MvcDemo/Data/Repositories/IOrderRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MvcDemo.Data.Repositories
+{
+    public interface IOrderRepository
+    {
+        IEnumerable<OrderSummary> GetOrderSummaries();
+    }
+}
---
+++ MvcDemo/Data/Repositories/OrderSummary.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace MvcDemo.Data.Repositories
+{
+    public class OrderSummary
+    {
+        public int OrderId { get; set; }
+
+        public string Customer { get; set; }
+
+        public decimal Total { get; set; }
+
+        public IEnumerable<string> Products { get; set; }
+    }
+}
